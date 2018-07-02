797f00df79ef60670d8cda87c30bf5f57ab05a6e
Abhisek Pattnaik <abhisekp@engineer.com>
2018-07-03 00:09:22 +0530

    Use index.js file for testing
    Use index.js file instead of relying on the "main" property in the
    package.json file.

--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const transformer = require('.');
+const transformer = require('./index');
 
 test('buble jest remove flow types', () => {
   expect(transformer.process('(num: number): number => 0', { all: true })).toMatchSnapshot();
